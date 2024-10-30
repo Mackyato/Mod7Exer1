6135bde8e6b14d22567e45e3ce2e65364be85be7
marksoberano <soberano.markanthony@auf.edu.ph>
2024-10-30 13:50:21 +0800

    SCREENSHOTS

---
+++ SCREENSHOT/INSERT_DELETE_SS/tblemployee.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 30, 2024 at 06:37 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `companydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblemployee`
+--
+
+CREATE TABLE `tblemployee` (
+  `EmployeeID` int(10) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Address` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `ContactNo` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblemployee`
+--
+
+INSERT INTO `tblemployee` (`EmployeeID`, `Name`, `Address`, `email`, `ContactNo`) VALUES
+(1, 'Mark Anthony Soberano', 'Mabalacat City', 'soberano.markanthony@auf.edu.ph', '0923451291'),
+(2, 'Derick Pangilinan', 'Telabastagan', 'derick@gmail.com', '0923451298'),
+(4, 'florenzo', 'Hooli', 'hooli@florenzo.com', '09856575143'),
+(6, 'tgdfxhxf', 'xfchjnfg', 'cfgxjmcghm', 'hcgjmgfj');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblemployee`
+--
+ALTER TABLE `tblemployee`
+  ADD PRIMARY KEY (`EmployeeID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblemployee`
+--
+ALTER TABLE `tblemployee`
+  MODIFY `EmployeeID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
